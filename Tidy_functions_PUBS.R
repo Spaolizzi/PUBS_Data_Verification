ea87a1671befd4c828347eec930ab6e486cbbda9
Sophie Paolizzi <sophie_paolizzi@Sophies-MacBook-Pro.local>
2021-08-27 11:37:33 -0400

    finizaling cannon tweaks

--- Tidy_functions_PUBS.R
+++ Tidy_functions_PUBS.R
@@ -6,7 +6,7 @@ tidy_cannon <- function(cannon_data) {
     mutate(picture.shield.currentitem, shield_size = as.numeric(parse_number(picture.shield.currentitem))) %>% ## make shield size numeric
     mutate_if(is.integer, as.numeric) %>% mutate(placementAngle, ifelse(outcomeindex == 1, NA, placementAngle)) %>%
     mutate(cond = as.factor(cond))
-  assign("cannon_data",cannon_data, envir = .GlobalEnv)
+  return(cannon_data)
 }
 
 discrep <- function(angmu, r) {
@@ -31,11 +31,14 @@ create_vars <- function(cannon_data, set_vars){
   if ("catch_miss" %in% set_vars) {
     cannon_data$catch_miss <- cannon_data$outcomeindex 
     for(r in 1:nrow(cannon_data)){
-      if (cannon_data$catch_miss[r] == "1"){
-        cannon_data$catch_miss[r] <- "noresp"
-        #cannon_data$PE_angmu[r] <- NA
-        #cannon_data$PE_raw[r] <- NA
-        #cannon_data$trial.placeshield_mouse.latency[r] <- NA
+      if (cannon_data$catch_miss[r] == 1){ ## check 
+        if(cannon_data$trial.placeshield_mouse.latency[r] == 2500){
+          cannon_data$catch_miss[r] <- "noresp"
+          cannon_data$PE_angmu[r] <- NA
+          cannon_data$Abs_PE[r] <- NA
+          cannon_data$placementAngle[r] <- NA
+          cannon_data$trial.placeshield_mouse.latency[r] <- NA
+        }
       } else if (cannon_data$catch_miss[r] == 5){
         cannon_data$catch_miss[r] <- "hit"
       } else if (cannon_data$catch_miss[r] == 6){
@@ -48,12 +51,43 @@ create_vars <- function(cannon_data, set_vars){
      cannon_data$changepoint[r] <- ifelse(cannon_data$StaySwitch[r] == 1, cannon_data$trialnum[r], NA)
       }
   }
-  
+  cannon_data$cond_num <- NA
   for(r in 1:nrow(cannon_data)){
-    cannon_data$cond_num <- NA
     cannon_data$cond_num[r] <- ifelse(cannon_data$cond[r] == "ODDBALL", 1, 2)
   }
  
   
-  assign("cannon_data",cannon_data, envir = .GlobalEnv)
+  return(cannon_data) ##Michael says risky
 }
+
+
+check_irreg <- function(cannon_data){
+  cannon_data$Irreg <- NA
+  for(r in 1:nrow(cannon_data)){
+    if (is.na(cannon_data$Abs_PE[r])) {
+      if(cannon_data$outcomeindex[r] != 1){
+        cannon_data$Irreg[r] <- "CHECK_NA"
+      } else {
+        cannon_data$Irreg[r] <- NA
+      }
+    } else {
+      if (cannon_data$Abs_PE[r] <= ((cannon_data$shield_size[r])/2) && cannon_data$hitmiss[r] == 0){
+        cannon_data$Irreg[r] <- "CHECK_MISS"
+      } else if (cannon_data$Abs_PE[r] >= 30 & cannon_data$hitmiss[r] == 1){
+        cannon_data$Irreg[r] <- "CHECK_HIT"
+      }
+    }
+  }
+  return(cannon_data)
+}
+
+
+
+
+
+
+
+
+
+
+
