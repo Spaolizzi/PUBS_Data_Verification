a411ac74e759ff7a87736f077db0cd2d41037f4c
Lab <lab@Labs-iMac.local>
2021-09-13 17:01:57 -0400

    work on reversal markdown data processing

--- Tidy_functions_PUBS.R
+++ Tidy_functions_PUBS.R
@@ -1,5 +1,5 @@
-tidy_cannon <- function(cannon_data) {
-  cannon_data <- cannon_data %>% 
+tidy_cannon <- function(data) {
+  data <- data %>% 
     select(!c(build, experimentName, list.Condition.currentvalue)) %>% 
     group_by(subject, time) %>% ## be by subject
     dplyr::filter(trialcode == "cannon_outcome") %>% filter(!practiceblock < 6) %>% ##only data from trials
@@ -9,9 +9,9 @@ tidy_cannon <- function(cannon_data) {
   
   #numeric columns 
   cols.num <- c("placementAngle","prev_placementAngle", "shield_size", "subject")
-  cannon_data[cols.num] <- sapply(cannon_data[cols.num],as.numeric)
-  sapply(cannon_data, class)
-  return(cannon_data)
+  data[cols.num] <- sapply(data[cols.num],as.numeric)
+  sapply(data, class)
+  return(data)
 }
 
 
@@ -42,82 +42,87 @@ discrep <- function(angmu, r) {
   return(phi)
 }
 
-create_vars <- function(cannon_data, set_vars){
+
+
+create_vars <- function(data, set_vars){
+  if (numbercorrect %in% set_vars){
+    data$consecutivecorrect <- 
+  }
   if("Abs_PE" %in% set_vars) {
-    cannon_data$Abs_PE <- NA
-    for(r in 1:nrow(cannon_data)){
-      if(is.na(cannon_data$placementAngle[r])){
-        cannon_data$Abs_PE[r] <- NA
-      } else if(is.na(cannon_data$outcome[r])){
-        cannon_data$Abs_PE[r] <- NA
+    data$Abs_PE <- NA
+    for(r in 1:nrow(data)){
+      if(is.na(data$placementAngle[r])){
+        data$Abs_PE[r] <- NA
+      } else if(is.na(data$outcome[r])){
+        data$Abs_PE[r] <- NA
       } else {
-        cannon_data$Abs_PE[r] <- discrep(cannon_data$outcome[r], cannon_data$placementAngle[r])
-      }
+        data$Abs_PE[r] <- discrep(data$outcome[r], data$placementAngle[r])
+      }desc
     }
   }
   if("PE_angmu" %in% set_vars){
-    cannon_data$PE_angmu <- NA
-    for(r in 1:nrow(cannon_data)){
-      if(is.na(cannon_data$placementAngle[r])){
+    data$PE_angmu <- NA
+    for(r in 1:nrow(data)){
+      if(is.na(data$placementAngle[r])){
         tmp <- NA
       } else {
-      tmp <- discrep(cannon_data$angmu[r], cannon_data$placementAngle[r])
-      cannon_data$PE_angmu[r] <- tmp}
+      tmp <- discrep(data$angmu[r], data$placementAngle[r])
+      data$PE_angmu[r] <- tmp}
     }
   }
   if ("catch_miss" %in% set_vars) {
-    cannon_data$catch_miss <- cannon_data$outcomeindex 
-    for(r in 1:nrow(cannon_data)){
-      if (cannon_data$catch_miss[r] == 1){ ## check 
-        if(cannon_data$trial.placeshield_mouse.latency[r] == 2500){
-          cannon_data$catch_miss[r] <- "noresp"
-          cannon_data$PE_angmu[r] <- NA
-          cannon_data$Abs_PE[r] <- NA
-          cannon_data$placementAngle[r] <- NA
-          cannon_data$trial.placeshield_mouse.latency[r] <- NA
+    data$catch_miss <- data$outcomeindex 
+    for(r in 1:nrow(data)){
+      if (data$catch_miss[r] == 1){ ## check 
+        if(data$trial.placeshield_mouse.latency[r] == 2500){
+          data$catch_miss[r] <- "noresp"
+          data$PE_angmu[r] <- NA
+          data$Abs_PE[r] <- NA
+          data$placementAngle[r] <- NA
+          data$trial.placeshield_mouse.latency[r] <- NA
         }
-      } else if (cannon_data$catch_miss[r] == 5){
-        cannon_data$catch_miss[r] <- "hit"
-      } else if (cannon_data$catch_miss[r] == 6){
-        cannon_data$catch_miss[r] <- "miss"
+      } else if (data$catch_miss[r] == 5){
+        data$catch_miss[r] <- "hit"
+      } else if (data$catch_miss[r] == 6){
+        data$catch_miss[r] <- "miss"
       } 
     }
   }
   if ("changepoint" %in% set_vars){
-    for(r in 1:nrow(cannon_data)){
-     cannon_data$changepoint[r] <- ifelse(cannon_data$StaySwitch[r] == 1, cannon_data$trialnum[r], NA)
+    for(r in 1:nrow(data)){
+     data$changepoint[r] <- ifelse(data$StaySwitch[r] == 1, data$trialnum[r], NA)
       }
   }
-  cannon_data$cond_num <- NA
-  for(r in 1:nrow(cannon_data)){
-    cannon_data$cond_num[r] <- ifelse(cannon_data$cond[r] == "ODDBALL", 1, 2)
+  data$cond_num <- NA
+  for(r in 1:nrow(data)){
+    data$cond_num[r] <- ifelse(data$cond[r] == "ODDBALL", 1, 2)
   }
-  return(cannon_data)
+  return(data)
 }
 
 
-check_irreg <- function(cannon_data){
-  cannon_data$Irreg <- NA
-  for(r in 1:nrow(cannon_data)){
-    if(is.na(cannon_data$Abs_PE[r])) {
-      if(cannon_data$outcomeindex[r] != 1){
-        cannon_data$Irreg[r] <- "CHECK_NA"
+check_irreg <- function(data){
+  data$Irreg <- NA
+  for(r in 1:nrow(data)){
+    if(is.na(data$Abs_PE[r])) {
+      if(data$outcomeindex[r] != 1){
+        data$Irreg[r] <- "CHECK_NA"
       } else {
-        cannon_data$Irreg[r] <- NA
+        data$Irreg[r] <- NA
       }
-    } else if(is.na(cannon_data$hitmiss[r])){
-      cannon_data$Irreg[r] <- NA
-    } else if(is.na(cannon_data$shield_size[r])) {
-      cannon_data$Irreg[r] <- NA
+    } else if(is.na(data$hitmiss[r])){
+      data$Irreg[r] <- NA
+    } else if(is.na(data$shield_size[r])) {
+      data$Irreg[r] <- NA
     } else {
-        if (cannon_data$Abs_PE[r] <= (cannon_data$shield_size[r]/2) && cannon_data$hitmiss[r] == 0){
-        cannon_data$Irreg[r] <- "CHECK_MISS"
-      } else if (cannon_data$Abs_PE[r] >= 30 & cannon_data$hitmiss[r] == 1){
-        cannon_data$Irreg[r] <- "CHECK_HIT"
+        if (data$Abs_PE[r] <= (data$shield_size[r]/2) && data$hitmiss[r] == 0){
+        data$Irreg[r] <- "CHECK_MISS"
+      } else if (data$Abs_PE[r] >= 30 & data$hitmiss[r] == 1){
+        data$Irreg[r] <- "CHECK_HIT"
       }
     }
   }
-  return(cannon_data)
+  return(data)
 }
 
 
