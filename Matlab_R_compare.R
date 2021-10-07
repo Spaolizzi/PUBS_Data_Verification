1716c72f161da6a3d499ba4a417866a577d07754
Lab <lab@Labs-iMac.local>
2021-10-07 16:38:42 -0400

    restructure folder. Updates to cannon_markdown layout

--- Matlab_R_compare.R
+++ Matlab_R_compare.R
@@ -15,7 +15,7 @@ df_applysel <- as.data.frame(cbind(res$anyUp, res$binX, res$sel)) %>% setNames(c
 
  resid_compare <- cbind(as.data.frame(res$STATS[11]), as.data.frame(fit$residuals)) %>% setNames(c("matlab", "R"))
  coeff_compare <- cbind(as.data.frame(res$STATS[1]), as.data.frame(fit$coefficients)) %>% setNames(c("matlab", "R"))
- se_compare <- cbind(as.data.frame(res$STATS[7]), as.data.frame(fit$model)) %>% setNames(c("matlab", "R"))
+ wt_compare <- cbind(as.data.frame(res$STATS[15]), as.data.frame(fit$weights)) %>% setNames(c("matlab", "R"))
 
   
  
