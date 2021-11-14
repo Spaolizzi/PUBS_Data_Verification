90c00de150e06c6a11571c92be5f53e6153e99b6
Sophie Paolizzi <sophie_paolizzi@Sophies-MacBook-Pro.local>
2021-11-14 13:56:31 -0500

    adding qualtRics package

---
+++ helper_scripts/QualtRics_API.R
@@ -0,0 +1,6 @@
+#Qualtrics_API
+
+qualtrics_api_credentials(api_key = "fR94LfqyjsoW6I688Uis6Yvh1fOsRhvyDJZ7WS2B", 
+                          base_url = "unc.az1.qualtrics.com",
+                          overwrite = TRUE,
+                          install = TRUE)
